fa80ab7ee6dcc58cf0d8b23a80d96b882d52074a
Carson Simms <143337976+Carson-Simms@users.noreply.github.com>
2023-12-09 14:07:42 -0500

    Added Spring Boot Backend

---
+++ spring-boot-backend/barrys-survival/src/test/java/com/example/barryssurvival/BarrysSurvivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barryssurvival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarrysSurvivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
